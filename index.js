572cd99b52dad2ad4dbd7d9f54f586ae40237b5a
Elijah Guerrero <elijahguerrero97@gmail.com>
2019-08-16 18:39:41 -0500

    updated css and js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const QA = [
         'A Russian spy'],
         correct_answer: 2,
         img:'http://www.scholastic.com/content/images/articles/sn_ts/sn_ts_011210_hdr.jpg',
+        alt: "Image of MLK Jr",
+        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
     },
     {
         question: "Which of the following is true about Adolf Hitler?",
@@ -18,6 +20,7 @@ const QA = [
         'He escaped to Argentina'],
         correct_answer: 3,
         img:'https://ichef.bbci.co.uk/news/976/cpsprodpb/C892/production/_107264315_d-daylandings.jpg',
+        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
     },
     {
         question: "How many buildings fell in new york on September 9, 2001 (9/11)?",
@@ -27,6 +30,7 @@ const QA = [
         '4'],
         correct_answer: 2,
         img:'https://amp.businessinsider.com/images/55f3279fbd86ef13008b9cbd-640-480.jpg',
+        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
     },
     {
         question: "The United States of America's Congress passed a constitutional ammendment that made what legal?",
@@ -36,6 +40,7 @@ const QA = [
         'Genocide'],
         correct_answer: 1,
         img:'https://www.lawliberty.org/wp-content/uploads/2018/06/We-the-People-768x384.jpg',
+        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
     },
     {
         question: "Prescott Bush, the father of former president George H.W. Bush and grandfather of former president George W. Bush, was:",
@@ -45,6 +50,7 @@ const QA = [
         'Was formerly a resident of Kazakhstan, ran for president there and lost, then imigratted to America'],
         correct_answer: 1,
         img:'https://upload.wikimedia.org/wikipedia/commons/0/09/George_H._W._Bush_inauguration.jpg',
+        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
     },
     {
         question: "What American president flew on a private jet, called the \"lolita express\", 26 times which was owned by child sex trafficer Jeff Epstein",
@@ -54,6 +60,7 @@ const QA = [
         'Donald J. Trump'],
         correct_answer: 0,
         img:'https://image.iol.co.za/image/1/process/620x349?source=https://inm-baobab-prod-eu-west-1.s3.amazonaws.com/public/inm/media/image/iol/2018/12/05/18397690/218-12-5T16635Z_874113619_RC15CF4CD3_RTRMADP_3_PEOPLE-BUSH.JPG',
+        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
     },
     {
         question: "In the United States, how long has blue denoted the left and red denoted the right?",
@@ -63,6 +70,7 @@ const QA = [
         'Since the year 2000'],
         correct_answer: 3,
         img:'http://historynet.com/wp-content/uploads/image/2012/ACWP/2012%2007/Lead%20for%20Honor.jpg',
+        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
     },
     {
         question: "What former American president distinctly ignored Supreme Court ruling?",
@@ -72,6 +80,7 @@ const QA = [
         'John F. Kenedy'],
         correct_answer: 2,
         img:'https://www.gannett-cdn.com/presto/2019/06/27/PDTF/a64a9823-e560-47ca-82e3-e7efea43e42b-GTY_AFP_1HW8V6.jpg?width=540&height=&fit=bounds&auto=webp',
+        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
     },
     {
         question: "The man who who shot and attempted to assisanate President Ronald Regan was?",
@@ -81,6 +90,7 @@ const QA = [
         'A family friend of Ronald Regan\'s vice president, George H.W. Bush'],
         correct_answer: 3,
         img:'https://i0.wp.com/www.nationalreview.com/wp-content/uploads/2015/06/ronald-reagan-example-2.jpg?fit=788%2C460&ssl=1',
+        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
     },
     {
         question: "In the first half of the 1900s, the US government",
@@ -90,6 +100,7 @@ const QA = [
         'Made Tomatoes illegal because they believed it was poisonous'],
         correct_answer: 0,
         img:'https://kosublog.com/wp-content/uploads/2016/07/blog3-768x512.jpg',
+        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
     }
 ];
 
@@ -161,6 +172,12 @@ function show(element){
         console.log(`showing class: ${element}`);
     }
 }
+//--------------------- GET RANDOM FUNCTION --------------------//
+
+function getRandom(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 //------------------------ Main functions --------------------------------------//
 
 function renderQuiz(bool=false){
@@ -183,6 +200,9 @@ function startScreen(){
     questionNum=0;
     incorrect = 0;
     rand = 0;
+
+    $('body').css(`background-image`,`url(${QA[getRandom(QA.length)].img})`)
+    
     $('.question').replaceWith(`<span class="question">${questionNum}</span>`);
     $('.score').replaceWith(`<span class="score">${((questionNum-incorrect)/10)*100}</span>`);
 
@@ -252,7 +272,8 @@ function resultQuiz(bool=false){
             `<div class='result'>
                 <h2>Incorrect!</h2>
                 <p>The Correct answer was:
-                <span>"${QA[questionNum-1].answers[ QA[questionNum-1].correct_answer ]}"</span>
+                <a href=${QA[questionNum-1].src}>
+                "${QA[questionNum-1].answers[ QA[questionNum-1].correct_answer ]}"</a>
                 </p>
                 <button onclick="continueQuizButton()" type="button" class="nextButton">Continue</button>
             </div>`);
