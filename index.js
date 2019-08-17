bea4ca2a9ef2269fb5922935cdf2b7a0372a53cc
Elijah Guerrero <elijahguerrero97@gmail.com>
2019-08-16 20:01:21 -0500

    modified the opacity of the displays

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const QA = [
         'He escaped to Argentina'],
         correct_answer: 3,
         img:'https://ichef.bbci.co.uk/news/976/cpsprodpb/C892/production/_107264315_d-daylandings.jpg',
-        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
+        src: "https://vault.fbi.gov/adolf-hitler/adolf-hitler-part-01-of-04/view",
     },
     {
         question: "How many buildings fell in new york on September 9, 2001 (9/11)?",
@@ -30,7 +30,7 @@ const QA = [
         '4'],
         correct_answer: 2,
         img:'https://amp.businessinsider.com/images/55f3279fbd86ef13008b9cbd-640-480.jpg',
-        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
+        src: "https://www.nist.gov/pba/questions-and-answers-about-nist-wtc-7-investigation",
     },
     {
         question: "The United States of America's Congress passed a constitutional ammendment that made what legal?",
@@ -40,7 +40,7 @@ const QA = [
         'Genocide'],
         correct_answer: 1,
         img:'https://www.lawliberty.org/wp-content/uploads/2018/06/We-the-People-768x384.jpg',
-        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
+        src: "https://www.thoughtco.com/corwin-amendment-slavery-and-lincoln-4160928",
     },
     {
         question: "Prescott Bush, the father of former president George H.W. Bush and grandfather of former president George W. Bush, was:",
@@ -50,7 +50,7 @@ const QA = [
         'Was formerly a resident of Kazakhstan, ran for president there and lost, then imigratted to America'],
         correct_answer: 1,
         img:'https://upload.wikimedia.org/wikipedia/commons/0/09/George_H._W._Bush_inauguration.jpg',
-        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
+        src: "https://timeline.com/business-plot-overthrow-fdr-9a59a012c32a",
     },
     {
         question: "What American president flew on a private jet, called the \"lolita express\", 26 times which was owned by child sex trafficer Jeff Epstein",
@@ -60,7 +60,7 @@ const QA = [
         'Donald J. Trump'],
         correct_answer: 0,
         img:'https://image.iol.co.za/image/1/process/620x349?source=https://inm-baobab-prod-eu-west-1.s3.amazonaws.com/public/inm/media/image/iol/2018/12/05/18397690/218-12-5T16635Z_874113619_RC15CF4CD3_RTRMADP_3_PEOPLE-BUSH.JPG',
-        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
+        src: "https://www.washingtontimes.com/news/2016/may/14/bill-clinton-ditched-secret-service-on-multiple-lo",
     },
     {
         question: "In the United States, how long has blue denoted the left and red denoted the right?",
@@ -70,7 +70,7 @@ const QA = [
         'Since the year 2000'],
         correct_answer: 3,
         img:'http://historynet.com/wp-content/uploads/image/2012/ACWP/2012%2007/Lead%20for%20Honor.jpg',
-        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
+        src: "https://slate.com/culture/2018/04/why-democrats-are-blue-and-republicans-are-red.html",
     },
     {
         question: "What former American president distinctly ignored Supreme Court ruling?",
@@ -80,7 +80,7 @@ const QA = [
         'John F. Kenedy'],
         correct_answer: 2,
         img:'https://www.gannett-cdn.com/presto/2019/06/27/PDTF/a64a9823-e560-47ca-82e3-e7efea43e42b-GTY_AFP_1HW8V6.jpg?width=540&height=&fit=bounds&auto=webp',
-        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
+        src: "https://www.history.com/this-day-in-history/president-lincoln-suspends-the-writ-of-habeas-corpus-during-the-civil-war",
     },
     {
         question: "The man who who shot and attempted to assisanate President Ronald Regan was?",
@@ -90,7 +90,7 @@ const QA = [
         'A family friend of Ronald Regan\'s vice president, George H.W. Bush'],
         correct_answer: 3,
         img:'https://i0.wp.com/www.nationalreview.com/wp-content/uploads/2015/06/ronald-reagan-example-2.jpg?fit=788%2C460&ssl=1',
-        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
+        src: "https://whowhatwhy.org/2016/08/16/bush-angle-reagan-shooting-still-unresolved-hinckley-walks",
     },
     {
         question: "In the first half of the 1900s, the US government",
@@ -100,7 +100,7 @@ const QA = [
         'Made Tomatoes illegal because they believed it was poisonous'],
         correct_answer: 0,
         img:'https://kosublog.com/wp-content/uploads/2016/07/blog3-768x512.jpg',
-        src: "https://www.globalresearch.ca/court-decision-u-s-government-agencies-found-guilty-in-martin-luther-kings-assassination-2/5320024",
+        src: "https://www.snopes.com/fact-check/government-poison-10000-americans",
     }
 ];
 
